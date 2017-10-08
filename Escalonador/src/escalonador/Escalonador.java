3198a0a36682ddb8d29e9d9d177ec3a8987ae9e8
AmandaBiosca <32441448+AmandaBiosca@users.noreply.github.com>
2017-10-08 16:58:35 -0300

    Revert "Merge branch 'testeOrdem' of https://github.com/PBeani/Escalonador into testeOrdem"
    This reverts commit 973db47275c9ebeb709447a0f8f95f5795ae33a5, reversing
    changes made to fbb709aba16949befac40e3807779f0385cad59d.

--- Escalonador/src/escalonador/Escalonador.java
+++ Escalonador/src/escalonador/Escalonador.java
@@ -34,11 +34,6 @@ public class Escalonador {
         log = new Logfile();
         // t de 2 processos  executarem + t do turno que ele entrou
         tempoEspera = 3;
-<<<<<<< HEAD
-=======
-        nInstrucoes = 0;
-        nTrocas = 0;
->>>>>>> 6e2980191b365d2e234c0b476e72bb771b009c99
 
     }
 
@@ -168,6 +163,7 @@ public class Escalonador {
                     }
                     break;
             }
+            // ver o que precisa ser feito ao final de cada instrucao
             i++;
         }
         // terminou o ciclo sem executar e/s e sem finalizar a execucao
@@ -181,10 +177,6 @@ public class Escalonador {
             escreverLog.print(i);
             escreverLog.printf(" instrucoes");
             escreverLog.println("");
-<<<<<<< HEAD
-=======
-
->>>>>>> 6e2980191b365d2e234c0b476e72bb771b009c99
             log.atualizarMediaInstrucoes(i);
             p.bcp.setNumTrocas(p.bcp.getNumTrocas() + 1);
 
@@ -193,13 +185,10 @@ public class Escalonador {
     }
 
     private void rodarEscalonador() {
+        // fazer o funcionamento
         while (!tabelaProcessos.getTabelaProcesso().isEmpty()) {
             listaBloqueados.atualizarListaBloqueados(listaProntos);// incrementar na contagem do tempo
             listaProntos.atualizarStatus();
-<<<<<<< HEAD
-=======
-
->>>>>>> 6e2980191b365d2e234c0b476e72bb771b009c99
             if (tabelaProcessos.redistribuirCreditos()) {
                 redistribuirPrioridades();
             }
